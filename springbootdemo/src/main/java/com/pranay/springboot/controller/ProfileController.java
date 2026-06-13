524f406ddde2195f0cbfafd0add35f5372597222
panukale <pranaykale06@gmail.com>
2026-06-14 00:55:02 +0530

    Initial Spring Boot Project Setup

---
+++ springbootdemo/src/main/java/com/pranay/springboot/controller/ProfileController.java
@@ -0,0 +1,24 @@
+package com.pranay.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfileController {
+
+	@GetMapping("/name")
+	public String getName() {
+		return "Pranay";
+	}
+	
+	
+    @GetMapping("/role")
+	public String getRole() {
+		return "Java Backend Developer";
+	}
+	
+	@GetMapping("/target")
+	public String getTarget() {
+		return "15 LPA";
+	}
+}
