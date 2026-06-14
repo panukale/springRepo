63ff752d42d31fcb98fa4c6603bb102bb796b658
panukale <pranaykale06@gmail.com>
2026-06-14 12:55:32 +0530

    added path variable

--- springbootdemo/src/main/java/com/pranay/springboot/controller/HelloController.java
+++ springbootdemo/src/main/java/com/pranay/springboot/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.pranay.springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,11 @@ public class HelloController {
 	@GetMapping("/square")
 	public int getSquare(@RequestParam int number) {
 		return number*number;
+	}
+	
+	@GetMapping("/pathSquare/{num}")
+	public int pathSq(@PathVariable int num) {
+		return num*num;
 	}
 
 }
