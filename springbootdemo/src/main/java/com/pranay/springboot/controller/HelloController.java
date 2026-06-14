61e2486e5fbf0d705ce91121445275f1d9c06183
panukale <pranaykale06@gmail.com>
2026-06-14 12:46:21 +0530

    added req param

--- springbootdemo/src/main/java/com/pranay/springboot/controller/HelloController.java
+++ springbootdemo/src/main/java/com/pranay/springboot/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.pranay.springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello World this is pranay and we will rock the world in few days. be ready bro....";
+	}
+	
+	@GetMapping("/square")
+	public int getSquare(@RequestParam int number) {
+		return number*number;
 	}
 
 }
